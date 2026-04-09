8652470f5ecfd7cfe1f4203f6e786f76fb5d7de7
Mehedi Hassan Pranto <mehedi.h.prantoz@gmail.com>
2026-04-09 22:34:12 +0600

    fetching data fromclient

--- client/JS/login.js
+++ client/JS/login.js
@@ -13,6 +13,24 @@ const handleLogin = (event) => {
 };
 
 
-const fetchUserInfo = (user) => {
-    
-}
+const fetchUserInfo = async (user) => {
+    let data;
+    try {
+        const res = await fetch('http://localhost:5000/getUserInfo', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        }, 
+        body: JSON.stringify(user),
+    });
+
+    data = await res.json(); 
+
+    } catch (err) {
+        console.log("Error connecting to the server:", err);
+    }
+    finally {
+        console.log("User info from server: ", data);
+    }
+
+};
